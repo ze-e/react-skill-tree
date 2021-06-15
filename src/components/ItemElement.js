9d5b3e089e10777016409a96476707876819f692
zrr <zrexrodriguez@gmail.com>
2021-06-14 18:31:53 -0700

    added parents

--- src/components/ItemElement.js
+++ src/components/ItemElement.js
@@ -1,7 +1,7 @@
 function ItemElement(props) {
   const my = props.item;
   return(
-    <li className="item__content">{my.name} - {my.xp}, child of {my.parent && my.parent.name && my.parent.name}</li>
+    <li className="item__content">{my.name} - {my.xp} {my.parent && my.parent.name && `, child of ${my.parent.name}`}</li>
   )
 }
 
