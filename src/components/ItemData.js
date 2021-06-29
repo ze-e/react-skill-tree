0ba361c584948404054cba2d318fc61b302b4c46
zrr <zrexrodriguez@gmail.com>
2021-06-28 23:05:47 -0600

    feat/add limit of 3 lessons

--- src/components/ItemData.js
+++ src/components/ItemData.js
@@ -28,8 +28,8 @@ function ItemData(props) {
       {!editing ? <h5 className='item-data__name'>{my.name}</h5>:<input type="text" defaultValue={my.name} onChange={handleChangeName} onBlur={toggleEdit}></input>}
       <p className='item-data__XP'>XP : {my.xp}</p>
       <button className="item-data__edit" type="button" onClick={toggleEdit}>{editing ? 'Save' : 'Edit'}</button>
-      {my.children < 3 && <button className="item-data__add-child" type="button" onClick={handleAddChild}>Next Lesson</button>}
-      {props.lastItem && my.children < 3 && <button className="item-data__add-skill" type="button" onClick={handleAddLesson}>Add More Lessons</button>}
+      <button className="item-data__add-child" type="button" onClick={handleAddChild}>Next Lesson</button>
+      <button className="item-data__add-skill" type="button" onClick={handleAddLesson}>Add More Lessons</button>
 
     </li>
   )
